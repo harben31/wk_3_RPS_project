9fde4c2687c4c4ca6c3866a60cc0a9b0ab2ae27e
Benjamin Harris <harben31@gmail.com>
2021-02-14 09:45:55 -0800

    done! added toUpperCase method, changed the first if statement from (i||i||i) had to spell each option out. moved the scoreboard variable below the scoreing if else statement. enclosed most of the code in a main funtion.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,20 @@
 
 
-
     var RPS = ["R", "P","S"];
-
+    
     var scoreComp = 0;
     var scorePlayer = 0;
     var scoreTie = 0;
-    var scoreCard =( "\nyour score: " + scorePlayer +
-    "\nmy score: " + scoreComp +
-    "\nwe've tied " + scoreTie + " times."
-    );
-    // var scoreCard2 = scoreCard();
+    
+
+var mainFun = function(){
 
    var inputPrompt  = function() {
       var inInput = prompt("choose R P or S:", "put it here");
+      inInput = inInput.toUpperCase();
        return inInput;
+       console.log(inInput);
     };
-   
 
    var input = inputPrompt();
 
@@ -29,15 +27,61 @@
 
     var randomIndex = randomizer();
 
-    if (input === ("R" || "S" || "P")){
+    if (input === "S" ||
+        input === "R" ||
+        input === "P"){
         randomIndex;
-    } else {alert("you dont want to play with me? uwu")}
-
-
-    console.log(randomIndex);
+    } else {alert("you dont want to play with me? uwu");
+    return;
+};
 
     
-    // if (input === randomIndex) 
+
+    console.log(randomIndex);
+    alert("I picked " + randomIndex);
+
+    if (input === randomIndex) {
+        scoreTie++;
+        
+       var conTie = alert("You tied! Do you want to play again? ");
+       
+     } else if ((input === "R" && randomIndex === "S") 
+          || (input === "S" && randomIndex  === "P") 
+          || (input === "P" && randomIndex === "R")) 
+          {
+      scorePlayer++; 
+      var conWin = alert("You win! Do you want to play again? ");
+    
+      } else {
+      scoreComp++; 
+      var conLose = alert("You lose! Do you want to play again? ");
+    
+      };
+
+      console.log(scorePlayer);
+      console.log(scoreComp);
+      console.log(scoreTie);
+
+      var scoreCard =( "\nyour score: " + scorePlayer +
+    "\nmy score: " + scoreComp +
+    "\nwe've tied " + scoreTie + " times."
+    );  
+
+      
+      var playAgain = confirm("the score is:" + scoreCard);
+
+      if (playAgain){
+          mainFun();
+      }
+    }
+
+    
+
+    mainFun();
+
+
+
+        // if (input === randomIndex) 
     //     scoreTie++;
     //     if ((scoreTie.valueOf + 1)===scoreTie){
     //     confirm("You tied! Do you want to play again? " + scoreCard);
@@ -56,28 +100,3 @@
     //     confirm("You lose! Do you want to play again? " + scoreCard);
     //     if (confirm === true) {inputPrompt()};
     //     }};
-
-    if (input === randomIndex) {
-        scoreTie++;
-        
-        confirm("You tied! Do you want to play again? " + scoreCard);
-        // if (confirm === true) {inputPrompt()
-    }
-         else if ((input === "R" && randomIndex === "S") 
-            || (input === "S" && randomIndex  === "P") 
-            || (input === "P" && randomIndex === "R")) 
-            {
-        scorePlayer++; 
-        confirm("You win! Do you want to play again? " + scoreCard);
-        // if (confirm === true) {inputPrompt()};
-        } else {
-        scoreComp++; 
-        confirm("You lose! Do you want to play again? " + scoreCard);
-        // if (confirm === true) {inputPrompt()};
-        };
-
-        console.log(scorePlayer);
-        console.log(scoreComp);
-        console.log(scoreTie);
-
-    
